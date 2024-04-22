cbd1a5c452b9c72692bc03cb6a513c21b6947518
Elikem Kalitsi <101428026+elikemk@users.noreply.github.com>
2024-04-21 23:33:03 -0400

    FIX BUGS ADD USESLEEP AND DEBUGS IN VSCODE

--- dl.cpp
+++ dl.cpp
@@ -19,19 +19,20 @@ int inventory_count = 0;
 void *factory_thread(void *arg);
 void *warehouse_thread(void *arg);
 
-void print_thread(string& message){
-  cout << message << endl;
+void print_thread(const string& message) {
+    cout << message << endl;
 }
 
 void *factory_thread(void *arg){
-  while(true) 
+  while(true){
     if (!inventory_full)
     {
       donut_count++;
-      print_thread("F: Doughnut #" + to_string(donut_count) + " produced")
+      print_thread("F: Doughnut #" + to_string(donut_count) + " produced");
+      usleep(500000);
     
   }else {
-      print_thread("F: The Production was Halted D:");
+    print_thread("F: The Production was Halted D:");
   }
 }
  return NULL;
@@ -39,52 +40,59 @@ void *factory_thread(void *arg){
 
 void *warehouse_thread(void *arg) {
   while (true) {
-    if (dc >= 12) { // donut count 
-      ic++; //inventory count
-      dc -= 12;
-      print_thread("W: Box #" + to_string(ic) + "ready");
-      if (ic >= 10) {
-        ic = true; 
-        print_thread("W: ITS FULL!!!");e
+    if (donut_count >= 12) { // donut count 
+      inventory_count++; //inventory count
+      donut_count -= 12;
+      print_thread("W: Box #" + to_string(inventory_count) + " is ready");
+      if (inventory_count >= 10) {
+        inventory_count = true; 
+        print_thread("W: ITS FULL!!!");
       }
     }
+    usleep(100000);
   }
   return NULL;
 }
 
 
 
-main() {
-  pthread_t factory;
+int main() {
+  pthread_t factory, warehouse;
   int status;
 
-  status = pthread_create(&factory, NULL, factory_thread, NULL);
+ 	status = pthread_create(&factory, NULL, factory_thread, NULL);
   if (status != 0){
-    cerr << "Faiiled " << endl;
+    cerr << "Faiiled Factory " << endl;
     return 1;
   }
 
-  while(true) {
+status = pthread_create(&warehouse, NULL, warehouse_thread, NULL);
+  if (status != 0){
+    cerr << "Failed Warehouse  " << endl;
+    return 1;
+  }
+
+while(true) {
 
 int boxes2sell;
-cout << "Number of boxes to sell"
+cout << "Number of boxes to sell (and negative to quite): ";
 cin >> boxes2sell;
-
-if (boxes2sell < 0) break;
-ic = boxes2sell;
-print_thread("S: Thank you!" + to_string(boxes2sell);
-if (inventory_full && ic < 10 ) {
-  inventory_full = false;
-  print_thread("Space no free, resuming...");
-    else {
-      print_thread("not enough space");
-    
+if (boxes2sell <= inventory_count) {
+    inventory_count -= boxes2sell;
+    print_thread("S: Thank you! we sold " + to_string(boxes2sell) + " boxes");
+    if (inventory_full && inventory_count < 10) {
+        inventory_full = false;
+        print_thread("Space now free, resuming...");
     }
-}
+} else {
+    print_thread("Not enough inventory");
+}}
+
 pthread_cancel(factory);
 pthread_cancel(warehouse);
 pthread_join(factory, NULL);
 pthread_join(warehouse, NULL);
+
 
 return 0;
 
