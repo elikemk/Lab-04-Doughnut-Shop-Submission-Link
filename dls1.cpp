d05a2f8a008fdd92ee29a5876b9d3c9c0073bb43
Elikem Kalitsi <101428026+elikemk@users.noreply.github.com>
2024-04-21 23:12:26 -0400

    step 2

--- dls1.cpp
+++ dls1.cpp
@@ -4,24 +4,30 @@
 
 using namespace std;
 
-int donut_count = 0
+int donut_count = 0;
 void *factory_thread(void *arg) {
   while(true){
     donut_count++; 
-    cout << "The Donuts from Factory: Produced " << donut_count << " Donuts\n";
     usleep(500000); // this sleeps for 0.5 seconds so 500k microseonds 
   }
   return NULL;
 }
 
-main(){
+int main(){
+
+
   pthread_t factory; 
   int status; 
 
   status = pthread_create(&factory, NULL, factory_thread, NULL);
   if (status != 0) {
-    cerr << "Failed to create teh factory thread";
+    cerr << "Failed to create teh factory thread" << status << endl;
     return 1;
+  }
+
+  while(true){
+    cout << "This is the Main Total Produced = " << donut_count << endl;
+    usleep(100000);
   }
   pthread_join(factory, NULL);
   return 0;
